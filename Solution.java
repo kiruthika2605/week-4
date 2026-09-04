54a2917baaa4dd9d8ef7bbb234cce9e343bddbd4
kirthika <kn1428@srmist.edu.in>
2026-09-04 16:36:59 +0530

    Adding week 4 programs

---
+++ Solution.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    public int[][] threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<int[]> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Skip duplicate first elements
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            // Since array is sorted, no possible triplet after this
+            if (nums[i] > 0) {
+                break;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(new int[]{
+                        nums[i],
+                        nums[left],
+                        nums[right]
+                    });
+
+                    // Skip duplicate values
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
